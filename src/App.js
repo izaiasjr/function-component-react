3ea3bc35378c955424247134f3c85e81f23063eb
Izaias Junior <up1.izaiasjr@gmail.com>
2022-07-22 20:12:51 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main className="master">
+      <section>
+        <h1>Esta é a main</h1>
+      </section>
+    </main>
+  );
+}
